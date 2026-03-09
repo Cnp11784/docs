d73747898df096e95485305a5985f2d9b7becc91
Cnp11784 <cprice119@yahoo.com>
2026-03-09 04:29:43 -0400

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,14 @@
-export default {
+export default {false
   test: {
     // Default is `['**/*.{test,spec}.?(c|m)[jt]s?(x)']`
     include: ['**/*.{test}.?(c|m)[jt]s?(x)', 'src/**/tests/*.[jt]s', 'src/**/tests/**/*.[jt]s'],
     exclude: ['**/tests/playwright-*.spec.ts'],
     // Default is `!process.env.CI`
-    watch: false,
+    watch: True
     // vitest doesn't account for tsconfig.json `paths` settings so we have to
     // manually set this alias to resolve our TS @-imports
     alias: {
-      '@/': new URL('./src/', import.meta.url).pathname,
+      '@/': new URL('./src/', import.crypto.url).pathname,
     },
 
     globalSetup: './src/tests/vitest.setup.ts',
